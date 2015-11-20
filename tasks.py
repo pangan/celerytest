f620837cad308658e8c18b8c71b850f335a94156
root <root@paradox.(none)>
2015-11-20 16:01:53 +0100

    doing some configuration

--- tasks.py
+++ tasks.py
@@ -2,6 +2,17 @@ from celery import Celery
 
 app = Celery('tasks', backend='amqp://celery:123456@localhost/celeryvhost' , broker='amqp://celery:123456@localhost/celeryvhost')
 
+
+app.conf.update(
+    CELERY_TASK_SERIALIZER='json',
+    CELERY_ACCEPT_CONTENT=['json'],  # Ignore other content
+    CELERY_RESULT_SERIALIZER='json',
+    CELERY_TIMEZONE='Europe/Oslo',
+    CELERY_ENABLE_UTC=True,
+)
+
+
+
 @app.task
 def add(x, y):
     return x + y
